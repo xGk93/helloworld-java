2d364b8ba7a53f7bd599f7f02ff1fcca87f96d07
David <72227184+David12000@users.noreply.github.com>
2024-11-27 23:42:33 +0100

    Version initiale

---
+++ src/main/fr/iutrodez/helloworld/App.java
@@ -0,0 +1,7 @@
+package fr.iutrodez.helloworld;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions!");
+    }
+}
---
+++ src/test/fr/iutrodez/helloworld/AppTest.java
@@ -0,0 +1,16 @@
+package fr.iutrodez.helloworld;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+	
+	@Test
+    void testKoApp() {
+        assertTrue(false);
+    }
+}
